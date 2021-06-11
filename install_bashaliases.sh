c462dc2b9a5778b43b190337118358afe695e131
Zhou FENG <42152221+zfengg@users.noreply.github.com>
2021-06-12 02:43:36 +0800

    mv $TOOLKIT to compatible zsh

--- install_bashaliases.sh
+++ install_bashaliases.sh
@@ -3,3 +3,4 @@ currentDir=$(cd $(dirname "$0") && pwd)
 pathbashglobal="$currentDir/dotfiles/.bash_aliases_global"
 cp "$currentDir/dotfiles/.bash_aliases" ~/.bash_aliases
 sed -i "1 i source $pathbashglobal" ~/.bash_aliases
+sed -i "1 i export TOOLKIT=$currentDir" ~/.bash_aliases
