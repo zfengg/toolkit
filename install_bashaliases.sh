bebdb640fe875a361eda2a7d8ce0e5c77c620598
zfengg <zhoufeng@link.cuhk.edu.hk>
2021-08-17 14:18:03 +0800

    export /d/GitHub/toolkit/bin to PATH

--- install_bashaliases.sh
+++ install_bashaliases.sh
@@ -3,4 +3,5 @@ currentDir=$(cd $(dirname "$0") && pwd)
 pathbashglobal="$currentDir/dotfiles/.bash_aliases_global"
 cp "$currentDir/dotfiles/.bash_aliases" ~/.bash_aliases
 sed -i "1 i source $pathbashglobal" ~/.bash_aliases
+sed -i "1 i export PATH=\"\$PATH:\$TOOLKIT/bin\"" ~/.bash_aliases
 sed -i "1 i export TOOLKIT=$currentDir" ~/.bash_aliases
