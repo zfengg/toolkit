97451d81aa325e415c19c9e21df4357ac2f596a4
zfengg <42152221+zfengg@users.noreply.github.com>
2022-06-26 17:16:37 +0800

    revise 1D inhomo selfsimilar

--- lib/m/SelfSimilar1D.m
+++ lib/m/SelfSimilar1D.m
@@ -1,107 +1,102 @@
-%% To plot self-similar sets by iterating intervals
-% Tailored for homogeneous IFS for the sake of speed
+%% To plot homogeneous self-similar sets by iterating intervals
 % Zhou Feng @ 2022-5-20
 clc, clf, clear
 tic
 
 %% settings
-% IFS
-% ratios = [1/3, 1/3];
+ratios = [1/2, 1/3, 1/4];
+trans = [0, 1/3, 3/4];
+intervalInit = [0, 1];
+
+numItrs = 7; % iteration time
+
+% plot settings
+showTitle = true;
+showFirstItrs = true;
+numFirstItrs = numItrs;
+color = 'k';
+thickness = 30;
+
+%% examples
+% % mid-third Cantor set
+% ratios = [1/3 1/3];
 % trans = [0, 2/3];
 % intervalInit = [0, 1];
 
-numItrs = 7;
 
-isHomo = true;
-showTitle = true;
-colorPlot = 'black';
-thickness = 50;
+% % a classic example
+% ratios = [1/2, 1/3, 1/4];
+% trans = [0, 1/3, 3/4];
+% intervalInit = [0, 1];
 
-%% examples
-% example of Jian-Ci Xiao
-m = 5;
-a = 3;
-ratios = 1/3 * ones(1, m);
-trans = [0, a.^ - (0:1:(m - 2))];
-intervalInit = [0, 1.5];
-
-% mid-third Cantor set
-ratios = [1/3, 1/3];
-trans = [0, 2/3];
-intervalInit = [0, 1];
 
 %% prepare params & error handling
 isCompact = false;
-
 if length(ratios) == length(trans)
     isCompact = true;
 end
-
 if ~isCompact
     error('Illegal settings. Dimensions of the parameters does not match!')
 end
 
+spaceDim = 1;
 sizeIFS = length(ratios);
 trans = sort(trans);
 lenInit = diff(intervalInit);
+numFirstItrs = numFirstItrs + 1;
 
 %% generate points
-if isHomo
-    ptsInit = intervalInit(1);
-    ptsNow = ptsInit;
-    sizeNow = length(ptsNow);
-    ptsTotal = cell(numItrs + 1, 1);
-    ptsTotal{1} = ptsInit;
+ptsInit = intervalInit;
+ptsNow = ptsInit;
+sizeNow = length(ptsNow);
+ptsTotal = cell(numItrs + 1, 1);
+ptsTotal{1} = ptsInit;
 
-    for levelNow = 1:numItrs
-        ptsTmp = kron(ptsNow, ratios) + kron(ones(1, sizeNow), trans);
-        ptsNow = unique(ptsTmp);
-        sizeNow = length(ptsNow);
-        ptsTotal{levelNow + 1} = ptsNow;
+for levelNow = 1:numItrs
+    ptsTmp = zeros(spaceDim, sizeNow * sizeIFS);
+    for indexFct = 1:sizeIFS
+        ptsTmp((indexFct - 1) * sizeNow + 1:indexFct * sizeNow) = ...
+            ratios(indexFct) * ptsNow + trans(indexFct);
     end
-
+    ptsNow = ptsTmp;
+    sizeNow = length(ptsNow);
+    ptsTotal{levelNow + 1} = ptsNow;
 end
 
 %% plot
-if isHomo
-    r = ratios(1);
-    lenBasic = r^numItrs * lenInit;
-    ptsDiff = diff(ptsNow);
-    indexGaps = ptsDiff > lenBasic;
-    ptsLeft = ptsNow([true indexGaps]);
-    numSegments = length(ptsLeft);
-    ptsRight = [ptsNow([false, indexGaps]) - ptsDiff(indexGaps), ptsNow(end)] + lenBasic;
+ptsPlt = reshape(ptsNow, 2, []);
+numSegments = size(ptsPlt, 2);
 
-    figure(1)
-    plot([ptsLeft; ptsRight], zeros(2, numSegments), ...
-        colorPlot, "LineWidth", thickness)
-    set(gca, 'XColor', 'none', 'YColor', 'none')
+figure(1)
+plot(ptsPlt,...
+    zeros(2, numSegments), ...
+    color, "LineWidth", thickness)
+set(gca, 'XColor', 'none', 'YColor', 'none')
 
-    if showTitle
-        title(['Iteration time = ', num2str(numItrs)], 'Interpreter', 'latex');
+if showTitle
+    title(['Iteration time = ', num2str(numItrs)], 'Interpreter', 'latex');
+end
+
+if showFirstItrs && numItrs + 1 >= numFirstItrs
+    figure(2)
+    for i = 1:numFirstItrs
+        ptsPltTmp = reshape(ptsTotal{i}, 2, []);
+        numSegmentsTmp = size(ptsPltTmp, 2);
+        plot(ptsPltTmp, zeros(2, numSegmentsTmp) - i, ...
+                color, "LineWidth", thickness)
+        hold on
     end
-
+    hold off
+    set(gca, 'XColor', 'none', 'YColor', 'none', 'ZColor', 'none')
+    if showTitle
+        title(['Iteration time = ', num2str(numFirstItrs - 1 )], 'Interpreter', 'latex');
+    end
+    ylim([- numFirstItrs - 0.5 * thickness / 50, 0])
 end
 
 %% show param
 countPtsTotal = sizeNow;
-[~, countShapesTotal] = size(ptsLeft);
-tableResults = table(countPtsTotal, countShapesTotal);
+countIntervalsTotal = size(ptsPlt, 2);
+tableResults = table(countPtsTotal, countIntervalsTotal);
 format long
 disp(tableResults)
-
-% the right ends of first segments during iterations
-r = ratios(1);
-firstRights = lenInit * ones(numItrs + 1, 1);
-
-for i = 1:numItrs
-    lenBasic = r^i * lenInit;
-    ptsNow = ptsTotal{i + 1};
-    ptsDiff = diff(ptsNow);
-    indexFirstRight = find(ptsDiff > lenBasic, 1);
-    ptFirstRight = ptsNow(indexFirstRight + 1) - ptsDiff(indexFirstRight) + lenBasic;
-    firstRights(i + 1) = ptFirstRight;
-end
-
-fprintf("The right ends of leftmost segments:\n")
-disp(firstRights)
